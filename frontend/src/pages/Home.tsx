19d4410b0b8986402edd73d5470c4ca14c483b67
priteshsinghh <priteshkumarsingh@fxis.ai>
2026-02-03 12:26:42 +0530

    added cors and changed image

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -118,7 +118,7 @@ const Home = () => {
           >
             <div className="absolute inset-0 bg-gray-900/20 group-hover:bg-gray-900/10 transition-colors z-10"></div>
             <img
-              src="https://images.unsplash.com/photo-1696086152513-c74dc1d4b135?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0"
+              src="https://images.unsplash.com/photo-1615313439277-aa3a0ace266c?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Unisex Collection"
               loading="lazy"
               className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-110"
